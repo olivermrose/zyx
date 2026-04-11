8165de68305f7bb3ac60407dfc4b3826e4f480f8
Oliver Rose <hello@olivermrose.com>
2026-04-11 12:22:09 -0400

    feat: add camera

---
+++ src/lib/components/Camera.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	let video = $state<HTMLVideoElement>();
+
+	onMount(async () => {
+		if (!video) return;
+
+		const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+		video.srcObject = stream;
+
+		const { resolve } = Promise.withResolvers<void>();
+		video.onloadeddata = () => resolve();
+
+		video.play();
+	});
+</script>
+
+<video
+	class="absolute right-4 bottom-4 z-9999 aspect-video max-w-1/5 -scale-x-100 rounded-2xl border-2 border-neutral-500 object-cover"
+	autoplay
+	muted
+	playsinline
+	bind:this={video}
+></video>
